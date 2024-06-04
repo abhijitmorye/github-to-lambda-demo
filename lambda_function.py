0a7eed1d5054777efb57385797ac0e6772f2c993
abhijitmorye <abhijitmorye>
2024-06-04 16:27:44 -0400

    Added code for simple sns

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,23 @@
 import requests
 import pandas as pd
+import boto3
+
+sns_client = boto3.client('sns')
+sns_arn = 'arn:aws:sns:us-east-2:992382775813:amz-sns-topic-001'
 
 
 def lambda_handler(event, context):
-    url = 'http://echo.jsontest.com/key/value/one/100'
-    response = requests.get(url)
-    dataframe = pd.DataFrame(response.json())
-    print(dataframe)
+
+    try :
+        print('In try block now, accessing the url')
+        data = {"Key_1": [1, 2, 3], "key_2": [5, 6, 7]}
+        dataframe = pd.DataFrame(data)
+        print(dataframe)
+        message = "Dataframe created"
+        response = sns_client.publish(Subject="SUCCESS - Daily Data Processing",TargetArn=sns_arn, Message=message, MessageStructure='text')
+    except Exception as err:
+        print(err)
+        message = "Dataframe failed"
+        response = sns_client.publish(Subject="Failure - Daily Data Processing", TargetArn=sns_arn, Message=message,
+                                      MessageStructure='text')
+
